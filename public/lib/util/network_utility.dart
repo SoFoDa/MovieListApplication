8be87c0938d229ed27cc29b0ff0cd51fae282ac8
Johan von Hacht <johvh@kth.se>
2019-03-01 11:11:29 +0100

    added network utility class

---
+++ public/lib/util/network_utility.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class NetworkUtility {
+  // make the class singleton, i.e. only create one object and reuse.
+  static final NetworkUtility _netutil = new NetworkUtility._internal();
+  factory NetworkUtility() {
+    return _netutil;
+  }
+  NetworkUtility._internal();
+
+  final JsonDecoder _decoder = new JsonDecoder();
+
+  /// Makes a get request to url.
+  /// 
+  /// Returns json decoded result.
+  Future<dynamic> get(String url) {
+    return http.get(url).then((response) {
+      final String res = response.body;
+      final int statusCode = response.statusCode;
+
+      if (statusCode < 200 || statusCode > 400 || json == null) {
+        throw new Exception("Get request failed.");
+      }
+      return _decoder.convert(res);
+    });
+  }
+
+  /// Makes a post request to url.
+  /// 
+  /// Returns json decoded result.
+  Future<dynamic> post(String url, Map body) {
+    return http.post(url, body: body).then((response) {
+      final String res = response.body;
+      final int statusCode = response.statusCode;
+
+      if (statusCode < 200 || statusCode > 400 || json == null) {
+        throw new Exception("Post request failed.");
+      }
+      return _decoder.convert(res);
+    });
+  }
+}
