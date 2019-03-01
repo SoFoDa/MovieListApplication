43102e56cce90e74a743871663f226586a568037
David Johansson <davidjo2@kth.se>
2019-03-01 04:51:22 +0100

    stats_page placeholder and login button

--- public/lib/main.dart
+++ public/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './home_page.dart' as home_page;
+import './stats_page.dart' as stats_page;
 
 void main() => runApp(MyApp());
 
@@ -7,7 +8,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: _MovieListApp(),
+      home: _Login(),
+    );
+  }
+}
+
+class _Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Center(
+        child: RaisedButton(
+          child: Text('Log in'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => _MovieListApp()),
+            );
+          },
+        ),
+      ),
     );
   }
 }
@@ -36,21 +56,26 @@ class _MovieListAppState extends State<_MovieListApp> with SingleTickerProviderS
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Navigation"), 
+        title: new Text("MovieList"), 
         backgroundColor: Colors.redAccent,
-      bottom: new TabBar(
-          controller: controller,
+      ),  
+
+      bottomNavigationBar: new Material(
+        color: Colors.redAccent,
+        child: new TabBar(
+          controller: controller,          
           tabs: <Tab>[
-            new Tab(icon: new Icon(Icons.home)),
-            new Tab(icon: new Icon(Icons.settings)),            
+            new Tab(icon: new Icon(Icons.home)),                               
+            new Tab(icon: new Icon(Icons.account_circle)),               
           ]
         )
-      ),      
+      ),   
+
       body: new TabBarView(
         controller: controller,
         children: <Widget>[          
-          home_page.HomePage(),
-          home_page.HomePage(),
+          home_page.HomePage(),                     
+          stats_page.StatsPage(),          
         ]
       )
     );
