cb3e9dcaed45e36ebb40a262738de578f34deaca
Johan von Hacht <johvh@kth.se>
2019-03-09 16:36:24 +0100

    disable swipe navigator pop on homescreen

--- public/lib/views/home.view.dart
+++ public/lib/views/home.view.dart
@@ -53,28 +53,33 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
 
   @override
   Widget build(BuildContext context) {
-    return Container(  
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,            
-          stops: [0.1, 0.7, 0.9],
-          colors: [              
-            Color(0xFF245ADC),
-            Color(0xFF594CD2),
-            Color(0xFF913AC5),              
-          ],
-        ),
-      ),             
-      child: ListView.builder(
-        itemCount: activities.length,
-        itemBuilder: (context, index) {
-          return ListTile(    
-            title: checkPrevDate(activities[index].date, index),
-            subtitle: activity_card.ActivityCard(activities[index]),                                      
-          );
-        },
-      ),     
+    return new WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Container(  
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,            
+            stops: [0.1, 0.7, 0.9],
+            colors: [              
+              Color(0xFF245ADC),
+              Color(0xFF594CD2),
+              Color(0xFF594CD2),              
+            ],
+          ),
+        ),             
+        child: ListView.builder(
+          itemCount: activities.length,
+          itemBuilder: (context, index) {
+            return ListTile(    
+              title: checkPrevDate(activities[index].date, index),
+              subtitle: activity_card.ActivityCard(activities[index]),                                      
+            );
+          },
+        ),     
+      ),               
     );                
   }
 }
