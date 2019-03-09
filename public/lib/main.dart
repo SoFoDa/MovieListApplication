259c9e965a29ed5ad8d9fb65a557942b819990a4
Johan von Hacht <johvh@kth.se>
2019-03-09 16:41:02 +0100

    actually blocks nav pop on all home screens now

--- public/lib/main.dart
+++ public/lib/main.dart
@@ -67,49 +67,33 @@ class MovieListAppState extends State<MovieListApp> with SingleTickerProviderSta
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(      
-      appBar: _appBar(), 
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('TODO'),
-            ),
-            ListTile(
-              title: Text('item1'),
-            ),
-            ListTile(
-              title: Text('item2'),
-            ),
-            ListTile(
-              title: Text('item3'),
-            )
-          ],
-
-        ),
-      ),     
-      bottomNavigationBar: Material(
-        color: Color(0xFF133658),
-        child: new TabBar(
-          controller: tabController,          
-          tabs: <Tab>[
-            new Tab(icon: new Icon(Icons.home)),                               
-            new Tab(icon: new Icon(Icons.person)), 
-            new Tab(icon: new Icon(Icons.insert_chart)),                           
+    return new WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: new Scaffold(      
+        appBar: _appBar(),   
+        bottomNavigationBar: Material(
+          color: Color(0xFF133658),
+          child: new TabBar(
+            controller: tabController,          
+            tabs: <Tab>[
+              new Tab(icon: new Icon(Icons.home)),                               
+              new Tab(icon: new Icon(Icons.person)), 
+              new Tab(icon: new Icon(Icons.insert_chart)),                           
+            ]
+          )
+        ),   
+        body: TabBarView(
+          controller: tabController,
+          children: <Widget>[          
+            home.Home(),    
+            profile.Profile(),                  
+            stats.Stats(),                     
           ]
-        )
-      ),   
-
-      body: TabBarView(
-        controller: tabController,
-        children: <Widget>[          
-          home.Home(),    
-          profile.Profile(),                  
-          stats.Stats(),                     
-        ]
+        ),
       ),
-    );
+    );  
   }  
 
   PreferredSizeWidget _appBar() {
